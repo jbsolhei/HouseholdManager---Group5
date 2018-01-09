e8744061bfcdd87d20760322af25e6216c93948f
Magnus Conrad Hyll <magnus@hyll.no>
2018-01-09 15:20:54 +0100

    Started on Sessions, added JDBC dependency

---
+++ src/main/java/classes/Sessions.java
@@ -0,0 +1,25 @@
+package classes;
+
+import java.util.HashMap;
+
+public class Sessions {
+
+    private static final int timeoutSecs = 60
+    private static HashMap<String, Session> sessions;
+
+    private Sessions() {
+
+    }
+
+    public static Session getSession(String tokenID) {
+        Session session = sessions.get(tokenID);
+        if (session == null) {
+            return null;
+        }
+        else {
+            long now = System.currentTimeMillis() / 1000;
+
+            session.setLastActivityTimestamp();
+        }
+    }
+}
---
+++ src/main/java/classes/UserAuth.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class UserAuth {
+    /**
+     * Authenticates a user login attempt. Returns a session token ID if the
+     * @param email
+     * @param password
+     * @return
+     */
+    public static String authUser(String email, String password) {
+
+    }
+}
