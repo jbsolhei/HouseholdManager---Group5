3e97e8bb01ac6be2b3f6ae2c485fa0c0e952568f
Magnus Conrad Hyll <magnus@hyll.no>
2018-01-09 16:13:24 +0100

    Javadoc UserAuth and Sessions

--- src/main/java/classes/Sessions.java
+++ src/main/java/classes/Sessions.java
@@ -4,22 +4,32 @@ import java.util.HashMap;
 
 public class Sessions {
 
-    private static final int timeoutSecs = 60
+    private static final int timeoutSecs = 1800; // 30 min
     private static HashMap<String, Session> sessions;
 
     private Sessions() {
 
     }
 
-    public static Session getSession(String tokenID) {
-        Session session = sessions.get(tokenID);
+    /**
+     * Returns a session by its ID token. Returns null if the session doesn't exist
+     * or if it has timed out and has been removed.
+     * @param token the ID token for the session
+     * @return the Session object, or null if it doesn't exist or has timed out.
+     */
+    public static Session getSession(String token) {
+        Session session = sessions.get(token);
         if (session == null) {
             return null;
         }
         else {
             long now = System.currentTimeMillis() / 1000;
-
-            session.setLastActivityTimestamp(0);
+            if (now - session.getLastActivityTimestamp() > timeoutSecs) {
+                sessions.remove(token);
+                return null;
+            }
+            session.setLastActivityTimestamp(now);
+            return session;
         }
     }
 }
--- src/main/java/classes/UserAuth.java
+++ src/main/java/classes/UserAuth.java
@@ -2,12 +2,13 @@ package classes;
 
 public class UserAuth {
     /**
-     * Authenticates a user login attempt. Returns a session token ID if the
-     * @param email
-     * @param password
-     * @return
+     * Authenticates a user login attempt. Generates and returns a Session for this user if the login
+     * was successful, or null if the email/password combination was incorrect.
+     * @param email the user email address
+     * @param password the plain-text password
+     * @return a Session object, or null.
      */
-    public static String authUser(String email, String password) {
-        return null;
+    public static Session authUser(String email, String password) {
+        return null; // TODO
     }
 }
