04864540be11e3c4bc6fe131a6c8f576e8542823
tvgb <tvgb@outlook.com>
2018-01-10 10:32:30 +0100

    Utvidet UserDAO med flere funksjoner og laget skall for brukerautentisering is userAuth.

--- src/main/java/classes/UserAuth.java
+++ src/main/java/classes/UserAuth.java
@@ -1,5 +1,12 @@
 package classes;
 
+import database.DBConnector;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserAuth {
     /**
      * Authenticates a user login attempt. Generates and returns a Session for this user if the login
@@ -9,6 +16,29 @@ public class UserAuth {
      * @return a Session object, or null.
      */
     public static Session authUser(String email, String password) {
-        return null; // TODO
+        String query = "SELECT * FROM Person WHERE email = ? and password = ?";
+        boolean userAuthenticated = false;
+        DBConnector dbc = new DBConnector();
+
+        try {
+            Connection conn = dbc.getConn();
+            PreparedStatement st = conn.prepareStatement(query);
+            st.setString(1, email);
+            st.setString(2, password);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                userAuthenticated = true;
+            }
+
+            st.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dbc.disconnect();
+        }
+
+        return null;
     }
 }
