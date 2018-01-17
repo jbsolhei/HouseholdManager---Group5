612298b8ddad0dd0b976d4e85326caa3a0b9c74d
camilve <camilve@stud.ntnu.no>
2018-01-17 16:06:21 +0100

    FIks

--- src/main/webapp/js/addUser.js
+++ src/main/webapp/js/addUser.js
@@ -54,6 +54,12 @@ function addUser() {
             if(result === true) {
                 document.getElementById("alertbox").innerHTML = '<div class="alert alert-success">' +
                     '<strong>Success!</strong> You have now created a user.</div>';
+                $(".alert-success").fadeTo(3000, 500).slideUp(500, function(){
+                    $(".alert-danger").slideUp(500);
+                    $(function () {
+                        $('#theModal').modal('toggle');
+                    });
+                });
             } else {
                 document.getElementById("alertbox").innerHTML = '<div class="alert alert-danger">' +
                     '<strong>The email or phone number already exist</strong></div>';
