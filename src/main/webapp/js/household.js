3ddf036318bf6d525e7b8b05140982f3f9651b9f
camilve <camilve@stud.ntnu.no>
2018-01-24 21:32:00 +0100

    profile picture in household when pushing a person

--- src/main/webapp/js/household.js
+++ src/main/webapp/js/household.js
@@ -132,6 +132,13 @@ function showMiniProfile(index){
         }else {
             $("#toEditProfileButton").hide();
         }
+        var image;
+        if(members[index].profileImage != "" && members[index].profileImage != null) {
+            image = members[index].profileImage;
+        } else {
+            image = "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png";
+        }
+        $("#place_for_profile_pic").html("<img src='"+image+"' id='profile-img'>");
         document.getElementById("miniProfile-name").innerHTML = '<p id="miniProfile-name">' + members[index].name + '</p>';
         document.getElementById("miniProfile-email").innerHTML = '<p id="miniProfile-email">' + members[index].email + '</p>';
         document.getElementById("miniProfile-telephone").innerHTML = '<p id="miniProfile-telephone">' + members[index].telephone + '</p>';
