2598b1cc00e0e2a04fb3b11ce72099174d308264
Magnus Conrad Hyll <magnus@hyll.no>
2018-01-28 16:02:05 +0100

    Polished auth package javadoc

--- src/main/java/auth/package-info.java
+++ src/main/java/auth/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Framework for authentication and authorization when clients tries to
- * access REST-endpoints annotated with the @{@link auth.Auth} annotation.
+ * <p>Framework for authentication and authorization.</p>
+ * <p>Used when clients tries to access REST-endpoints annotated with the @{@link auth.Auth} annotation.</p>
  */
 package auth;
